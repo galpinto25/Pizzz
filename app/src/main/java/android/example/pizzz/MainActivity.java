58e7e370731d8febbb30e9e61621ceed57559b93
liormoalem <lior.moalem@mail.huji.ac.il>
2019-11-17 13:30:18 +0200

    added EntryActivity and MainActivity, and added buttons to the MainActivity

--- app/src/main/java/android/example/pizzz/MainActivity.java
+++ app/src/main/java/android/example/pizzz/MainActivity.java
@@ -1,79 +1,74 @@
 package android.example.pizzz;
-
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.ContextCompat;
-
-import android.content.Context;
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-
 
 public class MainActivity extends AppCompatActivity {
+
     private int mPrice = 0;
-    private Size mSize = Size.SMALL;
+    private PizzaSize mSize = PizzaSize.SMALL;
     private Button mTotalPrice;
     private ImageButton mSbutton;
     private ImageButton mMbutton;
     private ImageButton mLbutton;
-    private Boolean mSclicked;
+    private static final int smallPrice = 20;
+    private static final int mediumPrice = 40;
+    private static final int largePrice = 60;
+    private static final String currency = " NIS";
 
+    @SuppressLint("SetTextI18n")
+    protected void changePizzaSize(PizzaSize pizzaSize) {
+        if (pizzaSize == PizzaSize.SMALL) {
+            mSbutton.setImageResource(R.mipmap.small_clicked_foreground);
+            mPrice = smallPrice;
+        } else {
+            mSbutton.setImageResource(R.mipmap.small_not_clicked_foreground);
+        }
+        if (pizzaSize == PizzaSize.MEDIUM) {
+            mMbutton.setImageResource(R.mipmap.medium_clicked_foreground);
+            mPrice = mediumPrice;
+        } else {
+            mMbutton.setImageResource(R.mipmap.medium_not_clicked_foreground);
+        }
+        if (pizzaSize == PizzaSize.LARGE) {
+            mLbutton.setImageResource(R.mipmap.large_clicked_foreground);
+            mPrice = largePrice;
+        } else {
+            mLbutton.setImageResource(R.mipmap.large_not_clicked_foreground);
+        }
+        mSize = pizzaSize;
+        mTotalPrice.setText(mPrice + currency);
+    }
+
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mSbutton = (ImageButton) findViewById(R.id.s_button);
-        mMbutton = (ImageButton) findViewById(R.id.m_button);
-        mLbutton = (ImageButton) findViewById(R.id.l_button);
-        mSclicked = false;
-        mSize = Size.MEDIUM;
-        mMbutton.setImageResource(R.mipmap.medium_clicked_foreground);
-        mPrice = 40;
-        mTotalPrice = (Button) findViewById(R.id.total_price_button);
-        mTotalPrice.setText(mPrice + " NIS");
+        mSbutton = findViewById(R.id.s_button);
+        mMbutton = findViewById(R.id.m_button);
+        mLbutton = findViewById(R.id.l_button);
+        mTotalPrice = findViewById(R.id.total_price_button);
+        // todo ask Efrat about the default size (or empty)
+        changePizzaSize(PizzaSize.MEDIUM);
     }
 
+    @SuppressLint("SetTextI18n")
     public void ClickS(View view) {
-        mSclicked = !mSclicked;
-        if (mSize != Size.SMALL) {
-            mSbutton.setImageResource(R.mipmap.small_clicked_foreground);
-            mMbutton.setImageResource(R.mipmap.medium_not_clicked_foreground);
-            mLbutton.setImageResource(R.mipmap.large_not_clicked_foreground);
-            mPrice = 20;
-            mSize = Size.SMALL;
-            mTotalPrice = (Button) findViewById(R.id.total_price_button);
-            mTotalPrice.setText(mPrice + " NIS");
-        }
+        changePizzaSize(PizzaSize.SMALL);
     }
 
+    @SuppressLint("SetTextI18n")
     public void ClickM(View view) {
-//        mSclicked = !mSclicked;
-        if (mSize != Size.MEDIUM) {
-            mSbutton.setImageResource(R.mipmap.small_not_clicked_foreground);
-            mMbutton.setImageResource(R.mipmap.medium_clicked_foreground);
-            mLbutton.setImageResource(R.mipmap.large_not_clicked_foreground);
-
-            mPrice = 40;
-            mSize = Size.MEDIUM;
-            mTotalPrice = (Button) findViewById(R.id.total_price_button);
-            mTotalPrice.setText(mPrice + " NIS");
-        }
+        changePizzaSize(PizzaSize.MEDIUM);
     }
 
+    @SuppressLint("SetTextI18n")
     public void ClickL(View view) {
-        if (mSize != Size.LARGE) {
-            mSbutton.setImageResource(R.mipmap.small_not_clicked_foreground);
-            mMbutton.setImageResource(R.mipmap.medium_not_clicked_foreground);
-            mLbutton.setImageResource(R.mipmap.large_clicked_foreground);
-
-            mPrice = 60;
-            mSize = Size.LARGE;
-            mTotalPrice = (Button) findViewById(R.id.total_price_button);
-            mTotalPrice.setText(mPrice + " NIS");
-        }
+        changePizzaSize(PizzaSize.LARGE);
     }
 }
-
--- app/src/main/java/android/example/pizzz/PizzaSize.java
+++ app/src/main/java/android/example/pizzz/PizzaSize.java
@@ -1,5 +1,5 @@
 package android.example.pizzz;
 
-public enum Size{
+public enum PizzaSize {
     SMALL, MEDIUM, LARGE
 }
