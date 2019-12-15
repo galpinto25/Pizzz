dab1237e35b3bd86c429f90965e71ddecb7a3993
galium31 <gal.pinto@mail.huji.ac.il>
2019-12-15 13:23:14 +0200

    added documentation to Pizza and PizzaSize classes

--- app/src/main/java/android/example/pizzz/Pizza.java
+++ app/src/main/java/android/example/pizzz/Pizza.java
@@ -1,105 +1,207 @@
 package android.example.pizzz;
 
 import android.util.Pair;
-
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
-public class Pizza {
+/**
+ * A class which represents a Pizza object.
+ */
+class Pizza {
 
-    final static int ONION_PRICE = 1, MUSHROOMS_PRICE = 4, PEPPERONI_PRICE = 5, BASIL_PRICE = 3, OLIVES_PRICE = 3, EXTRA_CHEESE_PRICE = 6;
+    // the prices of the extras which can be added to a pizza:
+    final static int ONION_PRICE = 1, MUSHROOMS_PRICE = 4, PEPPERONI_PRICE = 5, BASIL_PRICE = 3,
+            OLIVES_PRICE = 3, EXTRA_CHEESE_PRICE = 6;
     final static int SMALL_PRICE = 25, MEDIUM_PRICE = 35, LARGE_PRICE = 45;
 
-    // static variable single_instance of type Singleton
-//    private static Pizza pizza = null;
-
-    // variable of type String
+    // class private variables declaration:
     private PizzaSize size;
     private int sizePrice = 0;
     private int extrasPrice = 0;
     private Map<PizzaExtra, Pair<Boolean, Integer>> extras = new HashMap<>();
     private int quantity = 1;
 
+    // package-private constructor used by the pizza factory.
+    Pizza() {
+        size = PizzaSize.NONE;
+        this.initializeExtrasHashMap();
+    }
+
+    /**
+     * Sets the pizza size.
+     * @param size a given pizza size.
+     */
     void setSize(PizzaSize size) {
         this.size = size;
     }
 
+    /**
+     * Sets the price of the pizza size.
+     * @param sizePrice a given price of pizza size.
+     */
     void setSizePrice(int sizePrice) {
         this.sizePrice = sizePrice;
     }
 
-    void setOnion(Boolean onion) {
-        extras.put(PizzaExtra.ONION, new Pair<Boolean, Integer>(onion, ONION_PRICE));
-    }
-
-    void setMushrooms(Boolean mushrooms) {
-        extras.put(PizzaExtra.MUSHROOMS, new Pair<Boolean, Integer>(mushrooms, MUSHROOMS_PRICE));
-    }
-
-    void setOlives(Boolean olives) {
-        extras.put(PizzaExtra.OLIVES, new Pair<Boolean, Integer>(olives, OLIVES_PRICE));
-    }
-
-    void setPepperoni(Boolean pepperoni) {
-        extras.put(PizzaExtra.PEPPERONI, new Pair<Boolean, Integer>(pepperoni, PEPPERONI_PRICE));
-    }
-
-    void setBasil(Boolean basil) {
-        extras.put(PizzaExtra.BASIL, new Pair<Boolean, Integer>(basil, BASIL_PRICE));
-    }
-
-    void setExtraCheese(Boolean extraCheese) {
-        extras.put(PizzaExtra.EXTRA_CHEESE, new Pair<Boolean, Integer>(extraCheese, EXTRA_CHEESE_PRICE));
-    }
-
+    /**
+     * Sets the price of the extras of the pizza.
+     * @param extras_price a given price of the extras of the pizza.
+     */
     void setExtrasPrice(int extras_price) {
         this.extrasPrice = extras_price;
     }
 
+    /**
+     * Sets the extra 'onion', to be added to the pizza or to be removed, according to
+     * the given state.
+     * @param onion a given state: 'true' of 'false'.
+     */
+    void setOnion(Boolean onion) {
+        extras.put(PizzaExtra.ONION, new Pair<>(onion, ONION_PRICE));
+    }
+
+    /**
+     * Sets the extra 'mushrooms', to be added to the pizza or to be removed, according to
+     * the given state.
+     * @param mushrooms a given state: 'true' of 'false'.
+     */
+    void setMushrooms(Boolean mushrooms) {
+        extras.put(PizzaExtra.MUSHROOMS, new Pair<>(mushrooms, MUSHROOMS_PRICE));
+    }
+
+    /**
+     * Sets the extra 'olives', to be added to the pizza or to be removed, according to
+     * the given state.
+     * @param olives a given state: 'true' of 'false'.
+     */
+    void setOlives(Boolean olives) {
+        extras.put(PizzaExtra.OLIVES, new Pair<>(olives, OLIVES_PRICE));
+    }
+
+    /**
+     * Sets the extra 'pepperoni', to be added to the pizza or to be removed, according to
+     * the given state.
+     * @param pepperoni a given state: 'true' of 'false'.
+     */
+    void setPepperoni(Boolean pepperoni) {
+        extras.put(PizzaExtra.PEPPERONI, new Pair<>(pepperoni, PEPPERONI_PRICE));
+    }
+
+    /**
+     * Sets the extra 'basil', to be added to the pizza or to be removed, according to
+     * the given state.
+     * @param basil a given state: 'true' of 'false'.
+     */
+    void setBasil(Boolean basil) {
+        extras.put(PizzaExtra.BASIL, new Pair<>(basil, BASIL_PRICE));
+    }
+
+    /**
+     * Sets the extra 'extra_cheese', to be added to the pizza or to be removed, according to
+     * the given state.
+     * @param extraCheese a given state: 'true' of 'false'.
+     */
+    void setExtraCheese(Boolean extraCheese) {
+        extras.put(PizzaExtra.EXTRA_CHEESE, new Pair<>(extraCheese, EXTRA_CHEESE_PRICE));
+    }
+
+    /**
+     * Sets the quantity of the pizza.
+     * @param newQuantity a given quantity.
+     */
+    void setQuantity(int newQuantity)
+    {
+        quantity = newQuantity;
+    }
+
+    /**
+     * Gets the size of the pizza.
+     * @return the pizza size.
+     */
     PizzaSize getSize() {
         return size;
     }
 
-    public int getSizePrice() {
+    /**
+     * Gets the price of the pizza size.
+     * @return the price of the size of the pizza.
+     */
+    int getSizePrice() {
         return sizePrice;
     }
 
-    Boolean isOnion() {
-        return extras.get(PizzaExtra.ONION).first;
-    }
-
-    Boolean isMushrooms() {
-        return extras.get(PizzaExtra.MUSHROOMS).first;
-    }
-
-    Boolean isOlives() {
-        return extras.get(PizzaExtra.OLIVES).first;
-    }
-
-    Boolean isPepperoni() {
-        return extras.get(PizzaExtra.PEPPERONI).first;
-    }
-
-    Boolean isBasil() {
-        return extras.get(PizzaExtra.BASIL).first;
-    }
-
-    Boolean isExtraCheese() {
-        return extras.get(PizzaExtra.EXTRA_CHEESE).first;
-    }
-
+    /**
+     * Gets the price of the pizza extras.
+     * @return the price of the extras of the pizza.
+     */
     int getExtrasPrice() {
         return extrasPrice;
     }
 
+    /**
+     * Gets the total price of the pizza.
+     * @return the total price of the pizza.
+     */
     int getTotalPrice() {
         return (extrasPrice + sizePrice) * quantity;
     }
 
+    /**
+     * Gets the state of the extra 'onion'.
+     * @return 'true' if the pizza includes 'onion', 'false' otherwise.
+     */
+    Boolean isOnion() {
+        return Objects.requireNonNull(extras.get(PizzaExtra.ONION)).first;
+    }
+
+    /**
+     * Gets the state of the extra 'mushrooms'.
+     * @return 'true' if the pizza includes 'mushrooms', 'false' otherwise.
+     */
+    Boolean isMushrooms() {
+        return Objects.requireNonNull(extras.get(PizzaExtra.MUSHROOMS)).first;
+    }
+
+    /**
+     * Gets the state of the extra 'olives'.
+     * @return 'true' if the pizza includes 'olives', 'false' otherwise.
+     */
+    Boolean isOlives() {
+        return Objects.requireNonNull(extras.get(PizzaExtra.OLIVES)).first;
+    }
+
+    /**
+     * Gets the state of the extra 'pepperoni'.
+     * @return 'true' if the pizza includes 'pepperoni', 'false' otherwise.
+     */
+    Boolean isPepperoni() {
+        return Objects.requireNonNull(extras.get(PizzaExtra.PEPPERONI)).first;
+    }
+
+    /**
+     * Gets the state of the extra 'basil'.
+     * @return 'true' if the pizza includes 'basil', 'false' otherwise.
+     */
+    Boolean isBasil() {
+        return Objects.requireNonNull(extras.get(PizzaExtra.BASIL)).first;
+    }
+
+    /**
+     * Gets the state of the extra 'extra_cheese'.
+     * @return 'true' if the pizza includes 'extra_cheese', 'false' otherwise.
+     */
+    Boolean isExtraCheese() {
+        return Objects.requireNonNull(extras.get(PizzaExtra.EXTRA_CHEESE)).first;
+    }
+
+    /**
+     * @return a list which represents the extras which are included in the pizza.
+     */
     List<PizzaExtra> getExtras() {
-        List<PizzaExtra> list = new ArrayList<PizzaExtra>();
+        List<PizzaExtra> list = new ArrayList<>();
         for (Map.Entry entry : extras.entrySet()) {
             Pair pair = (Pair) entry.getValue();
             if (pair.first.equals(true)) {
@@ -109,52 +211,39 @@ public class Pizza {
         return list;
     }
 
-    void setExtras(Map<PizzaExtra, Pair<Boolean, Integer>> newExtras)
-    {
-        extras = newExtras;
-    }
-
+    /**
+     * @return a copy of the extras map of the pizza.
+     */
     Map<PizzaExtra, Pair<Boolean, Integer>> getExtrasForCopy()
     {
         return extras;
     }
 
-    void setQuantity(int newQuantity)
-    {
-        quantity = newQuantity;
-    }
-
-    void incCount() {
-        quantity++;
-    }
-
-    void decCount() {
-        quantity--;
-    }
-
+    /**
+     * Gets the quantity of the pizza.
+     * @return the quantity of the pizza.
+     */
     int getQuantity() {
         return quantity;
     }
 
-    // private constructor restricted to this class itself
-    Pizza() {
-        size = PizzaSize.NONE;
-        this.initializeExtrasHashMap();
+    /**
+     * Increment the quantity of the pizza.
+     */
+    void incCount() {
+        quantity++;
     }
 
-//    /**
-//     * static method to create instance of Singleton class
-//     *
-//     * @return todo
-//     */
-//    public static Pizza getInstance() {
-//        if (pizza == null) {
-//            pizza = new Pizza();
-//            pizza.initializeExtrasHashMap();
-//        }
-//        return pizza;
-//    }
+    /**
+     * Decrement the quantity of the pizza.
+     */
+    void decCount() {
+        quantity--;
+    }
 
+    /**
+     * Resets the pizza attributes.
+     */
     void reset() {
         sizePrice = 0;
         extrasPrice = 0;
@@ -163,27 +252,33 @@ public class Pizza {
         this.initializeExtrasHashMap();
     }
 
+    /**
+     * Initializes the extras map to default.
+     */
     private void initializeExtrasHashMap() {
-        extras.put(PizzaExtra.ONION, new Pair<Boolean, Integer>(false, ONION_PRICE));
-        extras.put(PizzaExtra.MUSHROOMS, new Pair<Boolean, Integer>(false, MUSHROOMS_PRICE));
-        extras.put(PizzaExtra.OLIVES, new Pair<Boolean, Integer>(false, OLIVES_PRICE));
-        extras.put(PizzaExtra.PEPPERONI, new Pair<Boolean, Integer>(false, PEPPERONI_PRICE));
-        extras.put(PizzaExtra.BASIL, new Pair<Boolean, Integer>(false, BASIL_PRICE));
-        extras.put(PizzaExtra.EXTRA_CHEESE, new Pair<Boolean, Integer>(false, EXTRA_CHEESE_PRICE));
+        extras.put(PizzaExtra.ONION, new Pair<>(false, ONION_PRICE));
+        extras.put(PizzaExtra.MUSHROOMS, new Pair<>(false, MUSHROOMS_PRICE));
+        extras.put(PizzaExtra.OLIVES, new Pair<>(false, OLIVES_PRICE));
+        extras.put(PizzaExtra.PEPPERONI, new Pair<>(false, PEPPERONI_PRICE));
+        extras.put(PizzaExtra.BASIL, new Pair<>(false, BASIL_PRICE));
+        extras.put(PizzaExtra.EXTRA_CHEESE, new Pair<>(false, EXTRA_CHEESE_PRICE));
     }
 
     /**
-     * todo
-     *
+     * @return the description of extras of the pizza, line by line.
+     * If there are no extras, returns 'No extras'.
      */
     String getExtrasDescription() {
         if (!hasExtras()) {
-            return "no extras";
+            return "No extras";
         }
         StringBuilder description = new StringBuilder();
+        // loops over the extras map
         for (Map.Entry entry : extras.entrySet()) {
             Pair pair = (Pair) entry.getValue();
+            // check if the extra included
             if (pair.first.equals(true)) {
+                // changes the first letter to capital and append to the description
                 String extraOutput = entry.getKey().toString().toLowerCase().replace("_", " ");
                 description.append(extraOutput.substring(0, 1).toUpperCase()).append(extraOutput.substring(1)).append("\n");
             }
@@ -191,23 +286,32 @@ public class Pizza {
         return description.toString().trim();
     }
 
+    /**
+     * todo check if needed with Efrat
+     * @return string represents the size of the pizza.
+     */
     String getSizeDescription() {
         String pizzaSizeText = this.getSize().toString().toLowerCase();
         return pizzaSizeText.substring(0, 1).toUpperCase() + pizzaSizeText.substring(1);
     }
 
+    /**
+     * @return string represents the quantity of the pizza.
+     */
     String getQuantityDescription() {
         return Integer.toString(this.getQuantity());
     }
 
+    /**
+     * @return string represents the total price of the pizza.
+     */
     String getTotalPriceDescription() {
         return this.getTotalPrice() + " NIS";
     }
 
     /**
-     * check if there is extras on the pizza
-     *
-     * @return true if there is extra, otherwise false
+     * Checks if there are extras on the pizza.
+     * @return true if there is an extra, else false.
      */
     private boolean hasExtras() {
 
@@ -219,4 +323,5 @@ public class Pizza {
         }
         return false;
     }
+
 }
--- app/src/main/java/android/example/pizzz/PizzaSize.java
+++ app/src/main/java/android/example/pizzz/PizzaSize.java
@@ -1,5 +1,8 @@
 package android.example.pizzz;
 
+/**
+ * An enum class which represents a size of pizza.
+ */
 public enum PizzaSize {
     SMALL, MEDIUM, LARGE, NONE
 }
